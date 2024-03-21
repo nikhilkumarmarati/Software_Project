a73b060f35e5592110c84a303f65ac63ddb16c41
M Nikhil Kumar <maratinikhilkumar@gmail.com>
2024-03-21 10:10:30 +0530

    Added Clerkpages

--- frontend/src/Clerk_Home.js
+++ frontend/src/Clerk_Home.js
@@ -1,39 +1,17 @@
 import {Link} from 'react-router-dom';
 import { useEffect,useState } from 'react';
 
-const Clerkhome=()=>{
-    const [complaint,setComplaint]=useState(false);
-    const addcomplaint=()=>{
-       setComplaint(true);
-    }
-    return(
-      <div className="Clerkhome">
+const Clerk_Home=()=>{
+  
+  return(
+    <div className="Clerkhome">
         <h1>welcome clerk</h1>
-        {!complaint &&(
-          <button className='submitbutton' onClick={addcomplaint} > <span style={{ fontSize: '20px' }}>+</span>Complaint</button>
-        )}
-            
-        {complaint &&(
-            <div className="fromcomplaint">
-            <label >Complaint:</label>
-            <form >
-                <div className="textarea">
-                <textarea cols="30" rows="10" required></textarea>
-                </div>
-               <div className="location"><label >Location:</label>
-              <input type="text"  />
-              </div>
-          </form>
-            
-          <button className='submitbutton'>Submit </button>
-
-            </div>
-          
-        )}
-        
-      </div>
+        <Link to="/Clerk_Complaint" className='submitbutton Add_complaint'>
+          +Complaint
+        </Link>
+    </div>
       
     );
 }
 
-export  default Clerkhome ;
+export  default Clerk_Home ;
